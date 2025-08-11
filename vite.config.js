19e98f6ab72daba63f87ccd262e10622623ce026
Luana Ruscitti <ruscittiluanagmail.com>
2025-08-11 16:45:45 +0200

    tailwind package installed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+})
 
 // https://vite.dev/config/
-export default defineConfig({
+/*export default defineConfig({
   plugins: [react()],
-})
+})*/
